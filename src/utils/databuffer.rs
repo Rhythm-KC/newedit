2797d5f4ba55eb14561b2d8e80ffebfbbc197df6
Rhythm KC <rhythmkc7@gmail.com>
2024-03-28 03:14:11 -0400

    removed some unused functions from the module

--- src/utils/databuffer.rs
+++ src/utils/databuffer.rs
@@ -1,5 +1,3 @@
-use std::io::{self, Write}; 
-
 pub struct DataBuffer{
     data: Vec<u8>
 }
@@ -14,21 +12,12 @@ impl DataBuffer{
        DataBuffer {data: Vec::from(data)}
    }
 
-   fn size(&self) -> usize{
-       self.data.len()
-   }
-
    pub fn append(&mut self, byte_data: u8){
        self.data.push(byte_data);
    }
 
    pub fn append_all(&mut self, bytes: &[u8]){
         self.data.extend_from_slice(bytes);
-   }
-
-   pub fn append_buffer(&mut self, other: &DataBuffer){
-       self.data.extend(other.data.iter());
-
    }
 
    pub fn clear(&mut self){
@@ -46,13 +35,5 @@ impl DataBuffer{
 
    pub fn len(&self)-> usize{
        self.data.len()
-   }
-
-   pub fn write_to_stdout(&mut self) -> io::Result<()>{
-       let stdout = io::stdout();
-       let mut handle = stdout.lock();
-       handle.write(self.data.as_slice())?;
-       let _ = handle.flush();
-       Ok(())
    }
 }

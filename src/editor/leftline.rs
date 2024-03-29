e69bbc88d97507fdb7b6e23bff642c551be536e9
Rhythm KC <rhythmkc7@gmail.com>
2024-03-29 04:49:41 -0400

    left line has some style now

--- src/editor/leftline.rs
+++ src/editor/leftline.rs
@@ -1,3 +1,4 @@
+use crate::utils::{databuffer::DataBuffer, terminalcommands};
 
 pub struct Leftline{
     pub height: usize,
@@ -11,12 +12,13 @@ impl Leftline{
 
     pub fn add_left_line_data_to_buf(&self, buf: &mut DataBuffer)
     {
+        buf.append_all(terminalcommands::graphic_rendition_cmd(terminalcommands::Rendition::INVERTEDCOLORS).as_bytes());
         buf.append(b'~');
         if self.width > 1
         {
             let padding = " ".repeat(self.width - 1);
             buf.append_all(padding.as_bytes());
         }
-
+        buf.append_all(terminalcommands::graphic_rendition_cmd(terminalcommands::Rendition::DEFAULT).as_bytes());
     }
 }
